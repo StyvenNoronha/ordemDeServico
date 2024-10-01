c168ffae8801af9352ab0a2c40bf57b68e838a90
styven <styvenn16@gmail.com>
2024-10-01 09:47:53 -0300

    Criando o banco de dados

---
+++ src/bancoDeDados/BancoDeDados.js
@@ -0,0 +1,15 @@
+import fs from 'node:fs/promises'
+const ENDERECO_BANCO_DE_DADOS = new URL('banco.json', import.meta.url)
+export class BancoDeDados {
+    #BancoDeDados = {}
+    construtor(){
+        fs.readFile(ENDERECO_BANCO_DE_DADOS, 'utf8').then((data)=>{
+            this.#BancoDeDados = JSON.parse(data)
+        }).catch(()=>{
+            this.#persistir()
+        })
+    }
+    #persistir(){
+        fs.writeFile(ENDERECO_BANCO_DE_DADOS, JSON.stringify(this.#BancoDeDados))
+    }
+}
--- src/intermediario/rotaIntermediario.js
+++ src/intermediario/rotaIntermediario.js
@@ -1,10 +1,13 @@
 import { rotas } from "../rotas/index.js";
+import { BancoDeDados } from "../bancoDeDados/BancoDeDados.js";
+
+const bancoDeDados = new BancoDeDados()
 export function rotaIndermediario(requisicao, resposta){
  const rota =  rotas.find((rota)=>{
     return rota.method === requisicao.method && rota.path === requisicao.url
  })
  if(rota){
-    return rota.controller({requisicao, resposta})
+    return rota.controller({requisicao, resposta, bancoDeDados})
  }
  return resposta.writeHead(404).end()
 }
