4298ee5dc6eb4c1f451c292611d8037a137a79f6
styven <styvenn16@gmail.com>
2024-10-01 08:36:46 -0300

    criando a lista de rotas

---
+++ src/rotas/index.js
@@ -0,0 +1,5 @@
+import { ordem } from "./ordem";
+
+export const rotas = [
+    ...ordem
+]
---
+++ src/rotas/ordem.js
@@ -0,0 +1,9 @@
+export const ordem = [
+    {
+        method:"POST",
+        path:"/ordem",
+        controller:(requisicao, resposta)=>{
+            resposta.end("Rota de ordem");
+        }
+    }
+]
