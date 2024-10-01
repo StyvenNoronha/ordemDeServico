a7439f22579fcb35ead193116991845d7fb9e296
styven <styvenn16@gmail.com>
2024-10-01 09:30:59 -0300

    Criando o objeto de ticket

--- src/controles/ordem/criacao.js
+++ src/controles/ordem/criacao.js
@@ -1,3 +1,18 @@
-export function criacao({requisicao, resposta}){
-   return resposta.end("Rota de ordem");
+import{randomUUID} from"node:crypto"
+
+export function criacao({ requisicao, resposta }) {
+
+  const { equipamento, descricao, nome_usuario } = requisicao.body;
+
+  const ordem = {
+    id:randomUUID(),
+    equipamento,
+    descricao,
+    nome_usuario,
+    status:"aberto",
+    data_criacao: new Date(),
+    data_atualizacao: new Date(),
+  };
+
+  return resposta.end(JSON.stringify(ordem));
 }
